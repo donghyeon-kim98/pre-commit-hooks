c3367e637749770b04cbc7e791200a4d5b6f4f15
donghyeon.kim <donghyeon.kim@deliveryhero.co.kr>
2020-08-07 22:13:29 +0900

    VEN-757 add test case for helper

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+import subprocess
+
+import pytest
+
+
+class GitHelpers:
+    @staticmethod
+    def checkout(branch: str, create: bool = True):
+        cmd = ["git", "checkout"]
+        if create:
+            cmd.append("-b")
+        cmd.append(branch)
+        subprocess.run(cmd)
+
+    @staticmethod
+    def stage_new_file(filename: str = "a.file"):
+        with open(filename, "w") as f:
+            f.write("hello world")
+        subprocess.run(["git", "add", filename])
+
+    @staticmethod
+    def commit(commit_msg: str):
+        subprocess.run(["git", "commit", "-m", commit_msg])
+
+
+@pytest.fixture
+def temp_git_dir(tmpdir):
+    git_dir = tmpdir.join("git_dir")
+    subprocess.run(["git", "init", "--", str(git_dir)], capture_output=True)
+    return git_dir
+
+
+@pytest.fixture(scope="session")
+def commit_editmsg_ref():
+    return ".git/COMMIT_EDITMSG"
+
+
+@pytest.fixture(scope="session")
+def ticket_id():
+    return "YOGIYO-123"
+
+
+@pytest.fixture(scope="session")
+def branch_with_ticket_id(ticket_id):
+    return f"{ticket_id.lower()}-test-branch"
+
+
+@pytest.fixture(scope="session")
+def git_helpers():
+    return GitHelpers
---
+++ tests/test_helper.py
@@ -0,0 +1,45 @@
+import pytest
+
+from hooks.helper import get_commit_msg, get_current_branch_name, get_ticket_id, is_special_commit
+
+
+def test_return_expected_branch_name(temp_git_dir, branch_with_ticket_id, git_helpers):
+    with temp_git_dir.as_cwd():
+        # When: Checkout branch
+        git_helpers.checkout(branch_with_ticket_id)
+
+        # Then: Return name of current branch
+        assert get_current_branch_name() == branch_with_ticket_id
+
+
+def test_return_expected_commit_msg(temp_git_dir, commit_editmsg_ref, git_helpers):
+    with temp_git_dir.as_cwd():
+        # Given: Commit new file
+        git_helpers.stage_new_file()
+
+        expected_commit_msg = "commit msg"
+        git_helpers.commit(expected_commit_msg)
+
+        # Expect: Return return expected commit message
+        assert get_commit_msg(commit_editmsg_ref) == expected_commit_msg
+
+
+def test_return_ticket_id_in_branch_name(temp_git_dir, branch_with_ticket_id, ticket_id, git_helpers):
+    with temp_git_dir.as_cwd():
+        # When: Checkout to branch name with ticket id
+        git_helpers.checkout(branch_with_ticket_id)
+
+        # Then: Return ticket id in branch name
+        assert get_ticket_id(branch_with_ticket_id) == ticket_id
+
+
+@pytest.mark.parametrize("commit_msg", "general commit message")
+def test_return_false_if_general_commit(commit_msg):
+    # Expect: Return False
+    assert is_special_commit(commit_msg) is False
+
+
+@pytest.mark.parametrize("commit_msg", ("Merge ", "Revert ", "Revert:"))
+def test_return_true_if_special_commit(commit_msg):
+    # Expect: Return True
+    assert is_special_commit(commit_msg) is True
